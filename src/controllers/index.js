39a87cc748a4f8ce551267fbb8e374785ef2e4a6
MaiUbeid <ubead95@gmail.com>
2019-08-21 16:07:24 +0300

    fix controllers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
 const { client, server } = require('./error');
-const { get, post } = require('./home');
+const { get, addList } = require('./home');
 
 router.get('/', get);
+router.post('/addList', addList);
 
 router.all('*', client);
 router.use(server);
