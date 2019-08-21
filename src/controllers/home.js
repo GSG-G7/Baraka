1205efd7421c5dbed909e9f29545d9b0ac40f5eb
MaiUbeid <ubead95@gmail.com>
2019-08-21 09:53:45 +0300

    test routers

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,3 +1,3 @@
 module.exports = (req, res) => {
-  res.send('hi love u');
+  res.render('home', { title: 'Baraka' });
 };
--- test/routes.test.js
+++ test/routes.test.js
@@ -1,6 +1,26 @@
 const tape = require('tape');
+const supertest = require('supertest');
+const app = require('../src/app');
 
 tape('init routes test', t => {
   t.equal(1, 1, 'should pass');
   t.end();
+});
+
+tape('test success for / endpoint', t => {
+  supertest(app)
+    .get('/')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      if (err) {
+        t.error(err);
+        t.end();
+      } else {
+        console.log(res.text);
+        const isInclode = res.text.includes('Baraka');
+        t.equals(isInclode, true, 'should response');
+        t.end();
+      }
+    });
 });
