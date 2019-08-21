ba32d67e955eb76f84cb4aa94022fd5d5c14dc52
amoodaa <amoodaa@gmail.com>
2019-08-21 11:41:21 +0300

    tests database for insert and select all complete

--- src/models/queries/items.js
+++ src/models/queries/items.js
@@ -1,9 +1,12 @@
 const connection = require('../config/connection');
 
-const insert = (name, content, listId, userId) => {
+const insert = itemData => {
+  // eslint-disable-next-line camelcase
+  const { name, content, list_id, user_id } = itemData;
   const sql = {
-    text: 'INSERT INTO items (name) VALUES ($1, $2, $3, $4);',
-    values: [name, content, listId, userId]
+    text: 'INSERT INTO items (name, content, list_id, user_id) VALUES ($1, $2, $3, $4);',
+    // eslint-disable-next-line camelcase
+    values: [name, content, list_id, user_id]
   };
   return connection.query(sql);
 };
