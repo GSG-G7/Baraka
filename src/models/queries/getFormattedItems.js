cb23482eb97a96e7fb6e342c038cffef8821277e
amoodaa <amoodaa@gmail.com>
2019-08-21 14:37:27 +0300

    fixed again how the data is sent

--- src/models/queries/getFormattedItems.js
+++ src/models/queries/getFormattedItems.js
@@ -1,14 +1,21 @@
 const connection = require('../config/connection');
 
 const format = data => {
-  const array = [[]];
+  const array = [];
+  const array2 = [];
   let ele = 0;
   for (let i = 0; i < data.length; i += 1) {
     ele = data[i];
     if (array[ele.list_id - 1] === undefined) array[ele.list_id - 1] = [];
     array[ele.list_id - 1].push(ele);
   }
-  return array;
+  for (let i = 0; i < array.length; i += 1) {
+    array2.push({
+      listname: array[i][0].listname,
+      list: array[i]
+    });
+  }
+  return array2;
 };
 const getFormattedData = () => {
   const sql = `SELECT items.id,items.name AS title, content,users.name AS username,list_id,lists.name AS listName FROM items INNER JOIN users ON users.id = items.user_id INNER JOIN lists ON lists.id = items.list_id ORDER BY items.list_id`;
@@ -18,5 +25,6 @@ const getFormattedData = () => {
     .then(format)
     .catch(err => err);
 };
+getFormattedData();
 
 module.exports = getFormattedData;
